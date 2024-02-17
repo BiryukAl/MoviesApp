5f9b4140cdb5773779d7ddd92bb642a2882fa46c
BiryukAl <kerzemyak001@gmail.com>
2024-02-17 15:08:34 +0300

    Add Dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,8 @@
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.kotlinAndroid) apply false
+    alias(libs.plugins.androidLibrary) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.realm) apply false
 }
 true // Needed to make the Suppress annotation work for the plugins block
