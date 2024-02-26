47ab0e59d9d6e13e2c93fd666a15b306e6bb8888
BiryukAl <kerzemyak001@gmail.com>
2024-02-27 02:05:27 +0300

    Add Mock Data

--- core/db/src/main/java/ru/kpfu/itis/core/db/DbModule.kt
+++ core/db/src/main/java/ru/kpfu/itis/core/db/DbModule.kt
@@ -2,6 +2,7 @@ package ru.kpfu.itis.core.db
 
 import io.realm.kotlin.Realm
 import io.realm.kotlin.RealmConfiguration
+import io.realm.kotlin.ext.realmSetOf
 import org.koin.dsl.module
 import ru.kpfu.itis.core.db.realm.Film
 
@@ -28,4 +29,40 @@ fun provideRealmConfig(): RealmConfiguration =
     ).schemaVersion(
         BuildConfig.VERSION_DB
     )
+        .build()
+
+fun provideMockRealmConfig(): RealmConfiguration =
+    RealmConfiguration.Builder(
+        schema = setOf(
+            Film::class
+        )
+    ).schemaVersion(
+        BuildConfig.VERSION_DB
+    ).initialData {
+        copyToRealm(
+            Film().apply {
+                kinopoiskId = 263531
+                name = "Мстители"
+                previewUrl = "https://kinopoiskapiunofficial.tech/images/posters/kp_small/301.jpg"
+                imageUrl = "http://kinopoiskapiunofficial.tech/images/posters/kp/263531.jpg"
+                genres = realmSetOf("фантастика")
+                countries = realmSetOf("США")
+                year = 2012
+                description = "США, Джосс Уидон(фантастика)"
+            }
+        )
+        copyToRealm(
+            Film().apply {
+                kinopoiskId = 6039
+                name = "Маска"
+                previewUrl = "https://kinopoiskapiunofficial.tech/images/posters/kp/6039.jpg"
+                imageUrl = "https://kinopoiskapiunofficial.tech/images/posters/kp_small/6039.jpg"
+                genres = realmSetOf("комедия")
+                countries = realmSetOf("США")
+                year = 1994
+                description = "Скромный и застенчивый служащий банка чувствует себя "
+            }
+        )
+    }
+        .inMemory()
         .build()
