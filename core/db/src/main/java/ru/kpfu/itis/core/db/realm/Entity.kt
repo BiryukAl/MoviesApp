de704f70aaff499eede9043f7966f96bf57ddd02
BiryukAl <kerzemyak001@gmail.com>
2024-02-27 01:50:52 +0300

    Add annotation on all property in Entity.kt

--- core/db/src/main/java/ru/kpfu/itis/core/db/realm/Entity.kt
+++ core/db/src/main/java/ru/kpfu/itis/core/db/realm/Entity.kt
@@ -3,18 +3,36 @@ package ru.kpfu.itis.core.db.realm
 import io.realm.kotlin.ext.realmSetOf
 import io.realm.kotlin.types.RealmObject
 import io.realm.kotlin.types.RealmSet
+import io.realm.kotlin.types.annotations.PersistedName
 import io.realm.kotlin.types.annotations.PrimaryKey
 import org.mongodb.kbson.ObjectId
 
 class Film() : RealmObject {
+    @PersistedName("_id")
     var _id: ObjectId = ObjectId()
+
     @PrimaryKey
+    @PersistedName("kinopoiskId")
     var kinopoiskId: Int = 0
+
+    @PersistedName("name")
     var name: String = ""
+
+    @PersistedName("previewUrl")
     var previewUrl: String = ""
+
+    @PersistedName("imageUrl")
     var imageUrl: String = ""
+
+    @PersistedName("genres")
     var genres: RealmSet<String> = realmSetOf()
+
+    @PersistedName("countries")
     var countries: RealmSet<String> = realmSetOf()
+
+    @PersistedName("year")
     var year: Int = 0
+
+    @PersistedName("description")
     var description: String = ""
 }
